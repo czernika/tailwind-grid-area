0e3818bc792b8bbd3fda5516d85a4cc881067504
Aliakseyenka Ihar <aliha.devs@gmail.com>
2022-12-28 22:07:06 +0300

    ci: bump release version

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ module.exports = plugin(function({ matchUtilities, theme }) {
     matchUtilities(
         {
             'grid-area': (value, {modifier}) => {
+
+                // if there are both value and modifier
+                // modifier will override value anyway
+                // `grid-area-[header]/footer` will generate `grid-area: footer`
                 if (modifier !== null) {
                     value = modifier
                 }
